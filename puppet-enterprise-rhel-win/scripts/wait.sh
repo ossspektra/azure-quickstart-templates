3908b998c7611cc85f3dad74ee5a328b6362ed1b
PraveenAnil <praveendosth@gmail.com>
2018-06-05 15:24:52 +0530

    Update wait.sh

--- puppet-enterprise-rhel-win/scripts/wait.sh
+++ puppet-enterprise-rhel-win/scripts/wait.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+#Configure Puppet Enterprise on first boot
+#
+
+ME=$(basename "$0")
+
+# Terminate script with error message
+function die
+{
+    echo "$ME: $1" >&2
+    exit 1
+}
+
+function usage
+{
+  die "Usage: $0 [FQDN of vm]"
+}
+
+fqdn=$1
+if [ -z "${fqdn}" ];then
+  usage
+fi
+# Ensure local hostname is resolvable
+
+publicip=$(dig "${fqdn}" +short) || die "Unable to dig ${fqdn} to determine the public ip"
+
+cat << EOF >> /etc/hosts
+# Puppet Enterprise must resolve local hostname(s)
+${publicip} ${fqdn}
+EOF
+
+echo "Install started of PE" > /tmp/pe_install.log
+
+
+# Use configure-pe.rb to configure PE
+exec /opt/puppetlabs/puppet/bin/ruby /opt/puppetlabs/azure/bin/configure-pe.rb -m "${fqdn}" >> /tmp/pe_install.log 2>&1
+die "exec /opt/puppetlabs/azure/bin/configure-pe.rb failed"
+
 while :
 do
   curl -k -s https://localhost:8140/status/v1/services | python -c 'import json,sys;obj=json.load(sys.stdin);sys.exit(0) if (obj["pe-master"]["state"] == "running") else sys.exit(1);'
