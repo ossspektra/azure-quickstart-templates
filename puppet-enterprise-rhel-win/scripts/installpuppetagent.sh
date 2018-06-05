6b579f368b0ad7059471983090d8000102725905
PraveenAnil <praveendosth@gmail.com>
2018-06-05 13:49:05 +0530

    Update installpuppetagent.sh

--- puppet-enterprise-rhel-win/scripts/installpuppetagent.sh
+++ puppet-enterprise-rhel-win/scripts/installpuppetagent.sh
@@ -2,6 +2,10 @@
 puname="puppet"
 sed -i "2i$1 $2" /etc/hosts
 sed -i "2i$1 $puname" /etc/hosts
-rpm -ivh http://pm.puppetlabs.com/puppet-agent/2016.1.2/1.4.2/repos/el/7/PC1/x86_64/puppet-agent-1.4.2-1.el7.x86_64.rpm
-yum install puppet -y
+## Old Command ##
+#rpm -ivh http://pm.puppetlabs.com/puppet-agent/2016.1.2/1.4.2/repos/el/7/PC1/x86_64/puppet-agent-1.4.2-1.el7.x86_64.rpm
+rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm
+
+yum install puppet-agent -y
+export PATH=/opt/puppetlabs/bin:$PATH
 systemctl start puppet
