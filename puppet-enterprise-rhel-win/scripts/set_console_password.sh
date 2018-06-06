b047d07d724303835de7bdc0fb048a32098ea854
PraveenAnil <praveendosth@gmail.com>
2018-06-06 15:00:09 +0530

    Create set_console_password.sh

---
+++ puppet-enterprise-rhel-win/scripts/set_console_password.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+#
+# set_console_password.sh: Set Puppet Enterprise console password
+#
+
+ME=$(basename "$0")
+pass=$1
+
+# Terminate script with error message
+function die
+{
+    echo "$ME: $1" >&2
+    exit 1
+}
+
+# Check command-line arguments
+if [ -n "$1" ]; then
+    echo "Usage: $ME" >&2
+    exit 1
+fi
+
+# Check for root permissions
+[ $EUID -ne 0 ] && die "script requires root permissions"
+
+# Use configure-pe.rb to set console password
+exec /opt/puppetlabs/azure/bin/configure-pe.rb -i -p "${pass}"
+die "exec /opt/puppetlabs/azure/bin/configure-pe.rb failed"
